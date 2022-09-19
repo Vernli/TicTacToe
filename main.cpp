11bbc6569a7b93c317fc72a8bb998133b860ad27
Krajewski <dawid_krajewski@outlook.com>
2022-09-19 18:56:40 +0200

    Gameboard, Print

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
+
+char FirstPlayer = 'X';
+char SecondPlayer = 'O';
+
+std::vector<std::string> gameboard = { "| | | |", "| | | |", "| | | |"};
+
+void printGB(){
+	std::cout << " |-------| " << std::endl;
+	std::cout << "  " << gameboard[0] << "  "<< std::endl;
+	std::cout << "  " << gameboard[1] << "  "<< std::endl;
+	std::cout << "  " << gameboard[2] << "  "<< std::endl;
+	std::cout << " |-------| " << std::endl;
+	return;
+}
 
 using namespace std;
 
 int main()
-{
-	cout << "I'm fine!" << endl;
+{	
+	cout << "Tic Tac Toe" << endl;
+	printGB();
 	return 0;
 }
